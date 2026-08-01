84dabde50443618983cb8573a92d73e218f3772f
Elber Ribeiro <dynaumloko@gmail.com>
2026-08-01 19:39:44 -0400

    kubeside 1.1.0

--- Formula/kubeside.rb
+++ Formula/kubeside.rb
@@ -1,28 +1,28 @@
 class Kubeside < Formula
   desc "Kubernetes client scoped to the developer, not the cluster operator"
   homepage "https://kubeside.dynaum.com/"
-  version "1.0.1"
+  version "1.1.0"
   license "Apache-2.0"
 
   on_macos do
     on_arm do
-      url "https://github.com/dynaum/kubeside/releases/download/v1.0.1/kubeside_1.0.1_darwin_arm64.tar.gz"
-      sha256 "54698a11d666429c206e54439526b582f9a1082182f4fa324fa9569694c9591e"
+      url "https://github.com/dynaum/kubeside/releases/download/v1.1.0/kubeside_1.1.0_darwin_arm64.tar.gz"
+      sha256 "e4c2de72017413b23eec9ab196df59b7dfdd081839e71213dc0636cf8adc05da"
     end
     on_intel do
-      url "https://github.com/dynaum/kubeside/releases/download/v1.0.1/kubeside_1.0.1_darwin_amd64.tar.gz"
-      sha256 "3b723e8303ad92bceba54733279a315ee57f9e30f4a82ace6404ec9d344fcbd9"
+      url "https://github.com/dynaum/kubeside/releases/download/v1.1.0/kubeside_1.1.0_darwin_amd64.tar.gz"
+      sha256 "959ba9c0cb15443b957923e8dbaccfe75966babd7b766bf47d42a9fa02f74a8a"
     end
   end
 
   on_linux do
     on_intel do
-      url "https://github.com/dynaum/kubeside/releases/download/v1.0.1/kubeside_1.0.1_linux_amd64.tar.gz"
-      sha256 "a35d520543473d60ce33866b7642c0dee8bf725f99ccc1210584c9e85949e127"
+      url "https://github.com/dynaum/kubeside/releases/download/v1.1.0/kubeside_1.1.0_linux_amd64.tar.gz"
+      sha256 "8ba7d2df6e7bc190a03ad1fb9ea86dc609fcde6abac0fe0bb7ad722dbc79bbe5"
     end
     on_arm do
-      url "https://github.com/dynaum/kubeside/releases/download/v1.0.1/kubeside_1.0.1_linux_arm64.tar.gz"
-      sha256 "84c9ba9e07e0a63d8a7cedeca2d6e772dce5cc64f23031a8fcb4ddf2a804097a"
+      url "https://github.com/dynaum/kubeside/releases/download/v1.1.0/kubeside_1.1.0_linux_arm64.tar.gz"
+      sha256 "dba3bc3432a76efedb182e7612cef646f66a4c968113edbdf5e7c4708f2a1ef9"
     end
   end
 
